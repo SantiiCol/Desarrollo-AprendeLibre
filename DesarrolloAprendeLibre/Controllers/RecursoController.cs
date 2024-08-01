659343cf1d8424f1ada3fb71af985b19b9492012
jp240 <jperdomof24@gmail.com>
2024-07-31 22:59:06 -0500

    Juli y Daya: se hizo los estilos de primaria y secundaria

---
+++ DesarrolloAprendeLibre/Controllers/RecursoController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DesarrolloAprendeLibre.Models;
+
+namespace DesarrolloAprendeLibre.Controllers
+{
+    public class RecursoController : Controller
+    {
+
+        // Acción para la vista Primaria
+        public ActionResult Primaria()
+        {
+            return View();
+        }
+
+        // Acción para la vista Secundaria
+        public ActionResult Secundaria()
+        {
+            return View();
+        }
+
+        private readonly AplDbContext _context;
+
+        public RecursoController(AplDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Recurso
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Recursos.ToListAsync());
+        }
+
+        // GET: Recurso/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var recurso = await _context.Recursos
+                .FirstOrDefaultAsync(m => m.IdRecurso == id);
+            if (recurso == null)
+            {
+                return NotFound();
+            }
+
+            return View(recurso);
+        }
+
+        // GET: Recurso/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Recurso/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdRecurso,Titulo,Descripcion,Materia,Imagen")] Recurso recurso)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(recurso);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(recurso);
+        }
+
+        // GET: Recurso/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var recurso = await _context.Recursos.FindAsync(id);
+            if (recurso == null)
+            {
+                return NotFound();
+            }
+            return View(recurso);
+        }
+
+        // POST: Recurso/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdRecurso,Titulo,Descripcion,Materia,Imagen")] Recurso recurso)
+        {
+            if (id != recurso.IdRecurso)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(recurso);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RecursoExists(recurso.IdRecurso))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(recurso);
+        }
+
+        // GET: Recurso/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var recurso = await _context.Recursos
+                .FirstOrDefaultAsync(m => m.IdRecurso == id);
+            if (recurso == null)
+            {
+                return NotFound();
+            }
+
+            return View(recurso);
+        }
+
+        // POST: Recurso/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var recurso = await _context.Recursos.FindAsync(id);
+            if (recurso != null)
+            {
+                _context.Recursos.Remove(recurso);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RecursoExists(int id)
+        {
+            return _context.Recursos.Any(e => e.IdRecurso == id);
+        }
+    }
+}
