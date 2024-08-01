a004a4735c59ecfe03464ad5941aad6f1bad5eaf
jp240 <jperdomof24@gmail.com>
2024-07-31 23:58:40 -0500

    Juli y Daya: Adelanto de la segunda parte de recursos(primaaria y secundaria)

---
+++ DesarrolloAprendeLibre/Controllers/GradosController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DesarrolloAprendeLibre.Controllers
+{
+    public class GradosController : Controller
+    {
+        public ActionResult Primero()
+        {
+            return View();
+        }
+        public ActionResult Segundo()
+        {
+            return View();
+        }
+        public ActionResult Tercero()
+        {
+            return View();
+        }
+        public ActionResult Cuarto()
+        {
+            return View();
+        }
+        public ActionResult Quinto()
+        {
+            return View();
+        }
+        public ActionResult Sexto()
+        {
+            return View();
+        }
+        public ActionResult Septimo()
+        {
+            return View();
+        }
+        public ActionResult Octavo()
+        {
+            return View();
+        }
+        public ActionResult Noveno()
+        {
+            return View();
+        }
+        public ActionResult Decimo()
+        {
+            return View();
+        }
+        public ActionResult Once()
+        {
+            return View();
+        }
+
+    }
+}
